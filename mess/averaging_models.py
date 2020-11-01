222054b6949ca310b4c64b18cad0e2b217a1a9f3
kamclar <kamilaclar@gmail.com>
2020-11-01 14:57:00 +0000

    ntb 06

---
+++ mess/averaging_models.py
@@ -0,0 +1,149 @@
+import pickle
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from sklearn.neural_network import MLPRegressor
+from sklearn.neural_network import MLPClassifier
+
+import skopt
+import argparse
+from numpy import mean
+from skopt import gp_minimize
+from skopt.space import Integer
+from keras.optimizers import Adam
+from skopt.utils import use_named_args
+from sklearn.ensemble import RandomForestRegressor
+
+
+import xgboost as xgb
+import lightgbm as lgb
+
+from sklearn.linear_model import ElasticNet, Lasso,  BayesianRidge, LassoLarsIC
+from sklearn.ensemble import RandomForestRegressor,  GradientBoostingRegressor
+from sklearn.kernel_ridge import KernelRidge
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import RobustScaler
+from sklearn.base import BaseEstimator, TransformerMixin, RegressorMixin, clone
+from sklearn.model_selection import KFold, cross_val_score, train_test_split
+from sklearn.metrics import mean_squared_error
+  
+
+ 
+class StackingAveragedModels(BaseEstimator, RegressorMixin, TransformerMixin):
+    def __init__(self, base_models, meta_model, n_folds=5):
+        self.base_models = base_models
+        self.meta_model = meta_model
+        self.n_folds = n_folds
+        
+    def save(self, fileName):
+        f = open(fileName,"wb")
+        pickle.dump(self,f)
+        f.close()
+        
+    def load(fileName):
+        f = open(fileName,"rb")
+        obj = pickle.load(f)
+        f.close()
+        return obj
+        
+    load = staticmethod(load)
+
+    # We again fit the data on clones of the original models
+    def fit(self, X, y):
+        self.base_models_ = [list() for x in self.base_models]
+        self.meta_model_ = clone(self.meta_model)
+        kfold = KFold(n_splits=self.n_folds, shuffle=True, random_state=156)
+        
+        # Train cloned base models then create out-of-fold predictions
+        # that are needed to train the cloned meta-model
+        out_of_fold_predictions = np.zeros((X.shape[0], len(self.base_models)))
+        for i, model in enumerate(self.base_models):
+            for train_index, holdout_index in kfold.split(X, y):
+                instance = clone(model)
+                self.base_models_[i].append(instance)
+                instance.fit(X[train_index], y[train_index])
+                y_pred = instance.predict(X[holdout_index])
+                out_of_fold_predictions[holdout_index, i] = y_pred
+                
+        # Now train the cloned  meta-model using the out-of-fold predictions as new feature
+        self.meta_model_.fit(out_of_fold_predictions, y)
+        return self
+
+#Do the predictions of all base models on the test data and use the averaged predictions as 
+#meta-features for the final prediction which is done by the meta-model
+    def predict(self, X):
+        meta_features = np.column_stack([
+            np.column_stack([model.predict(X) for model in base_models]).mean(axis=1)
+            for base_models in self.base_models_ ])
+        return self.meta_model_.predict(meta_features)
+
+class AveragingModels(BaseEstimator, RegressorMixin, TransformerMixin):
+    def __init__(self, models):
+        self.models = models
+        
+    def save(self, fileName):
+        f = open(fileName,"wb")
+        pickle.dump(self,f)
+        f.close()
+        
+    def load(fileName):
+        f = open(fileName,"rb")
+        obj = pickle.load(f)
+        f.close()
+        return obj
+        
+    load = staticmethod(load)
+        
+    #define clones of the original models to fit the data in
+    def fit(self, X, Y):
+        self.models_ = [clone(x) for x in self.models]
+        
+        # Train cloned base models
+        for model in self.models_:
+            model.fit(X, Y)
+
+        return self
+    
+    #Npredictions for cloned models and averaging
+    def predict(self, X):
+        predictions = np.column_stack([
+            model.predict(X) for model in self.models_
+        ])
+        return np.mean(predictions, axis=1)
+    
+
+# # # # # # # # # # # 
+# MODELS
+# # # # # # # # # # # 
+#lasso = make_pipeline(RobustScaler(), Lasso(alpha =0.0005, random_state=1))
+#ENet = make_pipeline(RobustScaler(), ElasticNet(alpha=0.0005, l1_ratio=.9, random_state=3))
+lasso = Lasso(alpha =0.0005, random_state=1)
+ENet = ElasticNet(alpha=0.0005, l1_ratio=.9, random_state=1)
+KRR = KernelRidge(alpha=0.6, kernel='polynomial', degree=2, coef0=2.5)
+RF = RandomForestRegressor(max_depth=15, random_state=1)
+ML =  MLPRegressor(max_iter=500, 
+                    hidden_layer_sizes=(500, 250, 50))
+model_xgb = xgb.XGBRegressor(colsample_bytree=0.4603, gamma=0.0468, 
+                             learning_rate=0.05, max_depth=3, 
+                             min_child_weight=1.7817, n_estimators=2200,
+                             reg_alpha=0.4640, reg_lambda=0.8571,
+                             subsample=0.5213, silent=1,
+                             random_state=1, nthread=-1)
+model_lgb = lgb.LGBMRegressor(objective='regression',num_leaves=5,
+                              learning_rate=0.05, n_estimators=720,
+                              max_bin = 55, bagging_fraction = 0.8,
+                              bagging_freq = 5, feature_fraction = 0.2319,
+                              feature_fraction_seed=9, bagging_seed=9,
+                              min_data_in_leaf=6, min_sum_hessian_in_leaf = 11)
+GBoost = GradientBoostingRegressor(n_estimators=3000, learning_rate=0.05,
+                                   max_depth=4, max_features='sqrt',
+                                   min_samples_leaf=15, min_samples_split=10, 
+                                   loss='huber', random_state=1)
+                                   
+                                   
+                                   
+
+if __name__ == "__main__":
+    main()
